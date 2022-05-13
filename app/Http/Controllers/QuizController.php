9f063dd1b16c6af21308a1b4ca6c0fe29bc7e2ea
mark1101 <markgeffer@hotmail.com>
2022-05-13 15:08:28 -0300

    atualizacao de emails com envio de todas respostas e passagem de dados dos usuarios pela rota

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -2,11 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\emailTeste;
+use App\Models\Candidate;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class QuizController extends Controller
 {
-    public function quiz(){
-        return view('Quiz.mainquiz');
+    public function quiz(Request $request)
+    {
+        $user = Candidate::where('id', $request->id)->first();
+        return view('Quiz.mainquiz', [
+            'user' => $user
+        ]);
+    }
+
+    public function sendFormEmail(Request $request)
+    {
+        $whatsphone = preg_replace("/[^0-9]/", "", $request->user['phone']);
+        $link = $whatsphone;
+
+        Mail::to('markagenciahey@gmail.com')->send(
+            new emailTeste(
+                $request->user,
+                $request->sector,
+                $request->socials,
+                $request->questions,
+                $request->concept,
+                $link
+            )
+        );
     }
 }
--- resources/js/components/Quiz/QuizComponent.vue
+++ resources/js/components/Quiz/QuizComponent.vue
@@ -12,16 +12,50 @@
         <div class="col" style="text-align: -webkit-center">
           <div class="login-box" style="align-self: center">
             <form style="display: inline-grid">
-              <a @click="add(1)">
+              <a
+                @click="
+                  add(
+                    1,
+                    'Questão 1',
+                    'Seu site e dados na primeira página do Google'
+                  )
+                "
+              >
                 Seu site e dados na primeira página do Google
               </a>
-              <a @click="add(2)">
+              <a
+                @click="
+                  add(
+                    2,
+                    'Questão 1',
+                    'Apenas os dados básicos, endereços, telefone e horário de funcionamento da empresa'
+                  )
+                "
+              >
                 Apenas os dados básicos, endereços, telefone, horário de
                 funcionamento da sua empresa
               </a>
-              <a @click="add(2)"> Apenas as Rede sociais do seu negócio </a>
-              <a @click="add(3)"> Seus concorrentes nas primeiras posições </a>
-              <a @click="add(3)"> Sua empresa não aparece no Google </a>
+              <a
+                @click="
+                  add(2, 'Questão 1', 'Apenas as Redes sociais do seu negócio')
+                "
+              >
+                Apenas as Rede sociais do seu negócio
+              </a>
+              <a
+                @click="
+                  add(3, 'Questão 1', 'Seus concorrentes na primeira posição')
+                "
+              >
+                Seus concorrentes nas primeiras posições
+              </a>
+              <a
+                @click="
+                  add(3, 'Questão 1', 'Sua empresa não aparece no Google')
+                "
+              >
+                Sua empresa não aparece no Google
+              </a>
             </form>
           </div>
         </div>
@@ -39,15 +73,37 @@
         <div class="col" style="text-align: -webkit-center">
           <div class="login-box" style="align-self: center">
             <form style="display: inline-grid">
-              <a @click="add(1)">
+              <a
+                @click="
+                  add(
+                    1,
+                    'Questão 2',
+                    'Sim, possuo um funil trabalhando 24 horas por dia'
+                  )
+                "
+              >
                 Sim, possuo um funil trabalhando 24 horas por dia
               </a>
-              <a @click="add(2)">
+              <a
+                @click="
+                  add(
+                    2,
+                    'Questão 2',
+                    'Não, mas pretendo implementar um processo comercial utilizando a internet nos próximos meses'
+                  )
+                "
+              >
                 Não, mas pretendo implementar um processo comercial utilizando a
                 internet nos próximos meses</a
               >
-              <a @click="add(2)"> Não vejo necessidade atualmente </a>
-              <a @click="add(3)"> O que é um funil de vendas? </a>
+              <a
+                @click="add(2, 'Questão 2', 'Não vejo necessidade atualmente')"
+              >
+                Não vejo necessidade atualmente
+              </a>
+              <a @click="add(3, 'Questão 2', 'O que é um funil de vendas?')">
+                O que é um funil de vendas?
+              </a>
             </form>
           </div>
         </div>
@@ -267,9 +323,15 @@
         <div class="col" style="text-align: -webkit-center">
           <div class="login-box" style="align-self: center">
             <form style="display: inline-grid">
-              <a @click="add(1)"> Sim, tenho interações diárias </a>
-              <a @click="add(2)"> Sim, tenho postagens semanais </a>
-              <a @click="add(3)"> Não estou ativo atualmente </a>
+              <a @click="add(1, 'Questão 5', 'Sim, tenho interações diárias')">
+                Sim, tenho interações diárias
+              </a>
+              <a @click="add(2, 'Questão 5', 'Sim, tenho postagens semanais')">
+                Sim, tenho postagens semanais
+              </a>
+              <a @click="add(3, 'Questão 5', 'Não estou ativo atualmente')">
+                Não estou ativo atualmente
+              </a>
             </form>
           </div>
         </div>
@@ -287,11 +349,35 @@
         <div class="col" style="text-align: -webkit-center">
           <div class="login-box" style="align-self: center">
             <form style="display: inline-grid">
-              <a @click="add(1)">
+              <a
+                @click="
+                  add(
+                    1,
+                    'Questão 6',
+                    'Sim, tenho uma equipe interna que faz isso'
+                  )
+                "
+              >
                 Sim, tenho uma equipe interna que faz isso
               </a>
-              <a @click="add(2)"> Sim, tenho esse serviço terceirizado </a>
-              <a @click="add(3)"> Não, nunca vi nem comi, só ouço falar </a>
+              <a
+                @click="
+                  add(2, 'Questão 6', 'Sim, tenho esse serviço terceirizado')
+                "
+              >
+                Sim, tenho esse serviço terceirizado
+              </a>
+              <a
+                @click="
+                  add(
+                    3,
+                    'Questão 6',
+                    'Não, nunca nem vi, nem comi só ouço falar'
+                  )
+                "
+              >
+                Não, nunca vi nem comi, só ouço falar
+              </a>
             </form>
           </div>
         </div>
@@ -309,11 +395,29 @@
         <div class="col" style="text-align: -webkit-center">
           <div class="login-box" style="align-self: center">
             <form style="display: inline-grid">
-              <a @click="add(2)"> Menos de R$ 1.000,00 </a>
-              <a @click="add(2)"> Entre R$ 1.000,00 e R$ 2.000,00</a>
-              <a @click="add(1)"> Acima de R$ 2.000,00 </a>
-              <a @click="add(3)"> Não invisto porque não sei como fazer </a>
-              <a @click="add(3)"> Não invisto porque não acredito </a>
+              <a @click="add(2, 'Questão 7', 'Menos de R$ 1.000,00')">
+                Menos de R$ 1.000,00
+              </a>
+              <a
+                @click="add(2, 'Questão 7', 'Entre R$ 1.000,00 e R$ 2.000,00')"
+              >
+                Entre R$ 1.000,00 e R$ 2.000,00</a
+              >
+              <a @click="add(1, 'Questão 7', 'Acima de R$ 2.000,00')">
+                Acima de R$ 2.000,00
+              </a>
+              <a
+                @click="
+                  add(3, 'Questão 7', 'Não invisto porque não sei como fazer')
+                "
+              >
+                Não invisto porque não sei como fazer
+              </a>
+              <a
+                @click="add(3, 'Questão 7', 'Não invisto porque não acredito')"
+              >
+                Não invisto porque não acredito
+              </a>
             </form>
           </div>
         </div>
@@ -429,11 +533,11 @@
         <div class="col" style="text-align: -webkit-center">
           <div class="login-box" style="align-self: center">
             <form style="display: inline-grid">
-              <a @click="add(3)"> Menos de 10% </a>
-              <a @click="add(2)"> Até 30% </a>
-              <a @click="add(2)"> Até 50% </a>
-              <a @click="add(1)"> Mais de 50% </a>
-              <a @click="add(1)"> 100% </a>
+              <a @click="add(3, 'Questão 9', 'Menos de 10%')"> Menos de 10% </a>
+              <a @click="add(2, 'Questão 9', 'Até 30%')"> Até 30% </a>
+              <a @click="add(2, 'Questão 9', 'Até 50%')"> Até 50% </a>
+              <a @click="add(1, 'Questão 9', 'Mais de 50%')"> Mais de 50% </a>
+              <a @click="add(1, 'Questão 9', '100%')"> 100% </a>
             </form>
           </div>
         </div>
@@ -451,17 +555,43 @@
         <div class="col" style="text-align: -webkit-center">
           <div class="login-box" style="align-self: center">
             <form style="display: inline-grid">
-              <a @click="add(1)">
+              <a
+                @click="
+                  add(
+                    1,
+                    'Questão 10',
+                    'Sim, meu site tem menos de um ano e está otimizado'
+                  )
+                "
+              >
                 Sim, meu site tem menos de um ano e está otimizado
               </a>
-              <a @click="add(2)">
+              <a
+                @click="
+                  add(
+                    2,
+                    'Questão 10',
+                    'Acredito que sim, mas meu site já tem mais de dois anos'
+                  )
+                "
+              >
                 Acredito que sim, mas meu site já tem mais de dois anos
               </a>
-              <a @click="add(2)">
+              <a
+                @click="
+                  add(
+                    2,
+                    'Questão 10',
+                    'Meu site tem mais de dois anos e possívelmente está desatualizado'
+                  )
+                "
+              >
                 Meu site tem mais de dois anos e possivelmente está
                 desatualizado
               </a>
-              <a @click="add(3)"> Não possuo site atualmente </a>
+              <a @click="add(3, 'Questão 10', 'Não possuo site atualmente')">
+                Não possuo site atualmente
+              </a>
             </form>
           </div>
         </div>
@@ -481,21 +611,61 @@
         <div class="col" style="text-align: -webkit-center">
           <div class="login-box" style="align-self: center">
             <form style="display: inline-grid">
-              <a @click="generateReport(0.5)">
+              <a
+                @click="
+                  generateReport(
+                    0.5,
+                    'Questão 11',
+                    'Eu não tenho tempo, e acabo perdendo dinheiro por não implementar nada'
+                  )
+                "
+              >
                 Eu não tenho tempo, e acabo perdendo dinheiro por não
                 implementar nada
               </a>
-              <a @click="generateReport(0.5)">
+              <a
+                @click="
+                  generateReport(
+                    0.5,
+                    'Questão 11',
+                    'Não encontro mão de obra especializada.'
+                  )
+                "
+              >
                 Não encontro mão de obra especializada.
               </a>
-              <a @click="generateReport(0.6)">
+              <a
+                @click="
+                  generateReport(
+                    0.6,
+                    'Questão 11',
+                    'Não preciso vender mais pela internet'
+                  )
+                "
+              >
                 Não preciso vender mais pela internet
               </a>
-              <a @click="generateReport(0.6)">
+              <a
+                @click="
+                  generateReport(
+                    0.6,
+                    'Questão 11',
+                    'Não tenho dificuldade, estou ganhando dinheiro atualizando o marketing como parte da minha estratégia comercial'
+                  )
+                "
+              >
                 Não tenho dificuldades, estou ganhando dinheiro utilizando o
                 marketing como parte da minha estratégia comercial
               </a>
-              <a @click="generateReport(0.8)">
+              <a
+                @click="
+                  generateReport(
+                    0.8,
+                    'Questão 11',
+                    'Não disponho de verba voltada para investir em marketing e vendas'
+                  )
+                "
+              >
                 Não disponho de verba voltada para investir em marketing e
                 vendas
               </a>
@@ -513,6 +683,11 @@ export default {
   model: {
     prop: "checked",
   },
+
+  props: {
+    user: Object,
+  },
+
   data() {
     return {
       activeStep: 1,
@@ -520,8 +695,10 @@ export default {
       checkedSocial: [],
       checkedKnowledge: [],
       checkedField: [],
+      result: [],
     };
   },
+
   methods: {
     generateReport: function () {
       this.$swal({
@@ -532,26 +709,45 @@ export default {
         showConfirmButton: false,
       }).then(this.add());
     },
-    add(value) {
+    add(value, question, result) {
       if (this.activeStep < 11) {
+        if (question && result) {
+          this.result.push({
+            question: question,
+            result: result,
+          });
+        }
         this.activeStep = this.activeStep + 1;
         this.sum += value;
       } else {
-        if (this.sum <= 10) {
-          window.setTimeout(function () {
-            window.location.href = "/parabens";
-          }, 3000);
-        }
-        if (this.sum > 10 && this.sum <= 14) {
-          window.setTimeout(function () {
-            window.location.href = "/cuidado";
-          }, 3000);
-        }
-        if (this.sum >= 15) {
-          window.setTimeout(function () {
-            window.location.href = "/alerta";
-          }, 3000);
-        }
+        let payload = {
+          user: this.user,
+          sector: this.checkedField,
+          socials: this.checkedSocial,
+          questions: this.result,
+          concept: this.checkedKnowledge,
+        };
+
+        axios
+          .post("/send-form-email", payload)
+          .then((response) => {
+            if (this.sum <= 10) {
+              window.setTimeout(function () {
+                window.location.href = "/parabens";
+              }, 3000);
+            }
+            if (this.sum > 10 && this.sum <= 14) {
+              window.setTimeout(function () {
+                window.location.href = "/cuidado";
+              }, 3000);
+            }
+            if (this.sum >= 15) {
+              window.setTimeout(function () {
+                window.location.href = "/alerta";
+              }, 3000);
+            }
+          })
+          .catch((error) => {});
       }
     },
     remove() {
